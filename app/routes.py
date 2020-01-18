04abc2cf1142213317893b5bc9c4be58c5e7c7d4
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-01-18 12:59:46 +0530

    Add register route.

--- app/routes.py
+++ app/routes.py
@@ -46,3 +46,17 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
