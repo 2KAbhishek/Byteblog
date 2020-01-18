01fb87730196bb34c0236ca3fd3bb32c43526ec4
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-01-18 12:47:24 +0530

    Add forms.py

---
+++ app/forms.py
@@ -0,0 +1,5 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from app.models import User
+
