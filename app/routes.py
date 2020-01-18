f4695f478e2a744763c1347b2afd6d395ab323c3
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-01-18 12:55:35 +0530

    Add routes.py

---
+++ app/routes.py
@@ -0,0 +1,7 @@
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import login_user, logout_user, current_user, login_required
+from werkzeug.urls import url_parse
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
+from app.models import User
+
