d27e7d42cbbec4718aa28ec85046ed00b5624567
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-01-18 12:58:48 +0530

    Add login route.

--- app/routes.py
+++ app/routes.py
@@ -22,3 +22,21 @@ def index():
     ]
     return render_template('index.html', title='Home', posts=posts)
 
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
