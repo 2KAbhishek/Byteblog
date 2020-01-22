62d442a7fc6b9b36c4f3b33a635c979769c2b0bb
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-01-22 21:17:30 +0530

    Add about_me & last_seen to user model.

--- app/models.py
+++ app/models.py
@@ -9,6 +9,8 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
