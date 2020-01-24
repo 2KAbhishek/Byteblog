df25c88dcd78139b468baebf5f5c8b1ed3d96e68
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-01-24 15:11:42 +0530

    Add setUp and tearDown.

--- tests.py
+++ tests.py
@@ -2,3 +2,15 @@ from datetime import datetime, timedelta
 import unittest
 from app import app, db
 from app.models import User, Post
+
+class UserModelCase(unittest.TestCase):
+    def setUp(self):
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
