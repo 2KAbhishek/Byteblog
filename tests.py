ec3c49659f40002ced26282bb0e925540c06d332
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-01-24 15:19:18 +0530

    Add test for user avatars.

--- tests.py
+++ tests.py
@@ -18,5 +18,11 @@ class UserModelCase(unittest.TestCase):
         self.assertFalse(u.check_password('incorrecto'))
         self.assertTrue(u.check_password('correct-mundo'))
 
+    def test_avatar(self):
+        u = User (username='john-doe', email='john@example.com')
+        self.assertEqual(u.avatar(128), ('https://www.gravatar.com/avatar/'
+                                         'd4c74594d841139328695756648b6bd6'
+                                         '?d=identicon&s=128'))
+
 if __name__ == '__main__':
     unittest.main(verbosity=2)
