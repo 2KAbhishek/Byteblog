5d1307fb51676cd844712dd835812d735cc03758
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-01-24 15:32:23 +0530

    Add route for unfollowing users.

--- app/routes.py
+++ app/routes.py
@@ -101,3 +101,18 @@ def follow(username):
     db.session.commit()
     flash('You are following {}!'.format(username))
     return redirect(url_for('user', username=username))
+
+@app.route('/unfollow/<username>')
+@login_required
+def unfollow(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        flash('User {} not found.'.format(username))
+        return redirect(url_for('index'))
+    if user == current_user:
+        flash('You cannot unfollow yourself!')
+        return redirect(url_for('user', username=username))
+    current_user.unfollow(user)
+    db.session.commit()
+    flash('You are not following {}.'.format(username))
+    return redirect(url_for('user', username=username))
