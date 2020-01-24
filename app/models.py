02016d2174a836a7856611d7621e5a78d857c11a
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-01-24 14:59:33 +0530

    Add method to retreive followed user's posts.

--- app/models.py
+++ app/models.py
@@ -49,6 +49,13 @@ class User(UserMixin, db.Model):
         return self.followed.filter(
             followers.c.follower_id == user.id).count() > 0
 
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers,(followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())
+
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
