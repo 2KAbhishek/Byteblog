673842246688524a43fb08326e4a6845169da5e1
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-01-24 15:24:04 +0530

    Add test for follow/unfollow.

--- tests.py
+++ tests.py
@@ -23,6 +23,27 @@ class UserModelCase(unittest.TestCase):
         self.assertEqual(u.avatar(128), ('https://www.gravatar.com/avatar/'
                                          'd4c74594d841139328695756648b6bd6'
                                          '?d=identicon&s=128'))
+    def test_follow(self):
+        u1 = User(username='kitty-cat', email='cat@manndo.com')
+        u2 = User(username='doggo', email='dog@mando.com')
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.commit()
+        self.assertEqual(u1.followed.all(), [])
+        self.assertEqual(u1.followers.all(), [])
 
+        u1.follow(u2)
+        db.session.commit()
+        self.assertTrue(u1.is_following(u2))
+        self.assertEqual(u1.followed.count(), 1)
+        self.assertEqual(u1.followed.first().username, 'doggo')
+        self.assertEqual(u2.followers.count(), 1)
+        self.assertEqual(u2.followers.first().username, 'kitty-cat')
+
+        u1.unfollow(u2)
+        db.session.commit()
+        self.assertFalse(u1.is_following(u2))
+        self.assertEqual(u1.followed.count(), 0)
+        self.assertEqual(u2.followers.count(), 0)
 if __name__ == '__main__':
     unittest.main(verbosity=2)
