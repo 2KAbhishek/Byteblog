d57a3a964b94429f3b969ddc16f9f9bbfa72d288
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-01-24 15:28:32 +0530

    Add test for followed posts.

--- tests.py
+++ tests.py
@@ -45,5 +45,44 @@ class UserModelCase(unittest.TestCase):
         self.assertFalse(u1.is_following(u2))
         self.assertEqual(u1.followed.count(), 0)
         self.assertEqual(u2.followers.count(), 0)
+
+    def test_follow_posts(self):
+        # create four users
+        u1 = User(username='east', email='east@example.com')
+        u2 = User(username='west', email='west@example.com')
+        u3 = User(username='north', email='north@example.com')
+        u4 = User(username='south', email='south@example.com')
+        db.session.add_all([u1, u2, u3, u4])
+
+        # create four posts
+        now = datetime.utcnow()
+        p1 = Post(body="post from east", author=u1,
+                  timestamp=now + timedelta(seconds=1))
+        p2 = Post(body="post from west", author=u2,
+                  timestamp=now + timedelta(seconds=4))
+        p3 = Post(body="post from north", author=u3,
+                  timestamp=now + timedelta(seconds=3))
+        p4 = Post(body="post from south", author=u4,
+                  timestamp=now + timedelta(seconds=2))
+        db.session.add_all([p1, p2, p3, p4])
+        db.session.commit()
+
+        # setup the followers
+        u1.follow(u2)  # east follows west
+        u1.follow(u4)  # east follows south
+        u2.follow(u3)  # west follows north
+        u3.follow(u4)  # north follows south
+        db.session.commit()
+
+        # check the followed posts of each user
+        f1 = u1.followed_posts().all()
+        f2 = u2.followed_posts().all()
+        f3 = u3.followed_posts().all()
+        f4 = u4.followed_posts().all()
+        self.assertEqual(f1, [p2, p4, p1])
+        self.assertEqual(f2, [p2, p3])
+        self.assertEqual(f3, [p3, p4])
+        self.assertEqual(f4, [p4])
+
 if __name__ == '__main__':
     unittest.main(verbosity=2)
