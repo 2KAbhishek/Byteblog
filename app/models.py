2f14486e791c6f5ae602c7bc7a39dd4e8349c1bc
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-01-24 14:00:38 +0530

    Add followers association table.

--- app/models.py
+++ app/models.py
@@ -4,6 +4,11 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from hashlib import md5
 
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
