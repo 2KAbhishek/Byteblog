8f0537a1d5283520a3049a80602a167392181888
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-01-25 15:03:04 +0530

    Add pagination to user profile.

--- app/routes.py
+++ app/routes.py
@@ -23,12 +23,12 @@ def index():
         db.session.commit()
         flash('Your post has been shared!')
         return redirect(url_for('index'))
-    page = request.args.get('page', 1, tyoe=int)
+    page = request.args.get('page', 1, type=int)
     posts = current_user.followed_posts().paginate(
         page, app.config['POSTS_PER_PAGE'], False)
     next_url = url_for('index', page=posts.next_num) if posts.has_next else None
-    prev_url = url_for('index', page=post.prev_num) if posts.has_prev else None
-    return render_template('index.html', title='Home', form=form, posts=posts.items, next_url = next_url, prev_url = prev_url)
+    prev_url = url_for('index', page=posts.prev_num) if posts.has_prev else None
+    return render_template('index.html', title='Home', form=form, posts=posts.items, next_url=next_url, prev_url=prev_url)
 
 @app.route('/explore')
 @login_required
@@ -38,7 +38,7 @@ def explore():
         page, app.config['POSTS_PER_PAAE'], False)
     next_url = url_for('explore', page=posts.next_num) if posts.has_next else None
     prev_url = url_for('explore', page=posts.prev_num) if posts.has_prev else None
-    return render_template('index.html', title='Explore', posts=posts, next_url = next_url, prev_url = prev_url)
+    return render_template('index.html', title='Explore', posts=posts.items, next_url=next_url, prev_url=prev_url)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -82,11 +82,14 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {'author': user, 'body': 'Test Post 1'},
-        {'author': user, 'body': 'Test Post 2'}
-    ]
-    return render_template('user.html', user=user, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template('user.html', user=user, posts=posts.items, next_url=next_url, prev_url=prev_url)
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
@@ -131,4 +134,3 @@ def unfollow(username):
     db.session.commit()
     flash('You are not following {}.'.format(username))
     return redirect(url_for('user', username=username))
-
