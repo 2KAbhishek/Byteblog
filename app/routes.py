1c3a0eb097d269b5a9e1cbc4271231c73549dc90
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-01-26 00:36:27 +0530

    Fix typo

--- app/routes.py
+++ app/routes.py
@@ -36,7 +36,7 @@ def index():
 def explore():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.timestamp.desc()).paginate(
-        page, app.config['POSTS_PER_PAAE'], False)
+        page, app.config['POSTS_PER_PAGE'], False)
     next_url = url_for('explore', page=posts.next_num) if posts.has_next else None
     prev_url = url_for('explore', page=posts.prev_num) if posts.has_prev else None
     return render_template('index.html', title='Explore', posts=posts.items, next_url=next_url, prev_url=prev_url)
