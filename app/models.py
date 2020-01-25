681ae12afdd8edfbfdc0faf00acd1b1f9816c3f0
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-01-25 17:09:39 +0530

    Add password reset token verifier.

--- app/models.py
+++ app/models.py
@@ -63,6 +63,15 @@ class User(UserMixin, db.Model):
             {'reset_password': self.id, 'exp': time() + expires_in},
             app.config['SECTET_KEY'], algorithm='HS256').decode('utf-8')
 
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+            algorithms='HS256')['reset_password']
+        except:
+            return
+        return User.query.get(id)
+
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
