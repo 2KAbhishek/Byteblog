7c4a199af6134e39ffb171090a6238938098822e
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-01-25 14:22:32 +0530

    Add posts per page.

--- config.py
+++ config.py
@@ -13,3 +13,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['keshri.abhishek63@gmail.com']
+    POSTS_PER_PAGE = 10
