93e0963b01c7ad1abd264baa5b75adc9dcd4b3b8
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-01-25 14:46:40 +0530

    Add next and prev page url.

--- app/routes.py
+++ app/routes.py
@@ -26,7 +26,9 @@ def index():
     page = request.args.get('page', 1, tyoe=int)
     posts = current_user.followed_posts().paginate(
         page, app.config['POSTS_PER_PAGE'], False)
-    return render_template('index.html', title='Home', form=form, posts=posts.items)
+    next_url = url_for('index', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('index', page=post.prev_num) if posts.has_prev else None
+    return render_template('index.html', title='Home', form=form, posts=posts.items, next_url = next_url, prev_url = prev_url)
 
 @app.route('/explore')
 @login_required
@@ -34,7 +36,9 @@ def explore():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.timestamp.desc()).paginate(
         page, app.config['POSTS_PER_PAAE'], False)
-    return render_template('index.html', title='Explore', posts=posts)
+    next_url = url_for('explore', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) if posts.has_prev else None
+    return render_template('index.html', title='Explore', posts=posts, next_url = next_url, prev_url = prev_url)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
