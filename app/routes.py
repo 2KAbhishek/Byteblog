cfa66f80f09de65bb7f2ab1e59ade6c9a649a662
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-01-25 14:29:12 +0530

    Add pagination to index and explore.

--- app/routes.py
+++ app/routes.py
@@ -23,9 +23,18 @@ def index():
         db.session.commit()
         flash('Your post has been shared!')
         return redirect(url_for('index'))
-    posts = current_user.followed_posts().all()
-    return render_template('index.html', title='Home', form=form, posts=posts)
+    page = request.args.get('page', 1, tyoe=int)
+    posts = current_user.followed_posts().paginate(
+        page, app.config['POSTS_PER_PAGE'], False)
+    return render_template('index.html', title='Home', form=form, posts=posts.items)
 
+@app.route('/explore')
+@login_required
+def explore():
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAAE'], False)
+    return render_template('index.html', title='Explore', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -119,8 +128,3 @@ def unfollow(username):
     flash('You are not following {}.'.format(username))
     return redirect(url_for('user', username=username))
 
-@app.route('/explore')
-@login_required
-def explore():
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template('index.html', title='Explore', posts=posts)
