9144c57a291c76e155098c3ecd0dab7c1f200612
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-01-25 17:53:04 +0530

    Add password reset route.

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, flash, redirect, url_for, request
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.urls import url_parse
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm, ResetPasswordRequestForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm, ResetPasswordRequestForm, ResetPasswordForm
 from app.models import User, Post
 from datetime import datetime
 from app.email import send_password_reset_email
@@ -148,3 +148,18 @@ def reset_passowrd_request():
         flash('Check your email for password reset instructions.')
         return redirect(url_for('login'))
     return render_template('reset_password_request.html', title='Reset Password', form=form)
+
+@app.route('reset_passowrd/<token>', methods=['GET', 'POST'])
+def reset_passowrd(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    user = User.verify_reset_password_token(token)
+    if not user:
+        return redirect(url_for('index'))
+    form = ResetPasswordForm
+    if form.validate_on_submit():
+        user.set_password(form.password.data)
+        db.session.commit()
+        flash('Your password has been reset.')
+        return redirect(url_for('login'))
+    return render_template('reset_password.html', form=form)
