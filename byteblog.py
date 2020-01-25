37783bd0bc46f50962598c0fcf3336b1c5967766
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-01-25 23:17:34 +0530

    Add app.run for VS Code debugging.

--- byteblog.py
+++ byteblog.py
@@ -5,3 +5,7 @@ from app.models import User, Post
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}
+
+# For debugging in VS Code
+if __name__ == '__main__':
+    app.run(use_debugger=False, use_reloader=False, passthrough_errors=True)
