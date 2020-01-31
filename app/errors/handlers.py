e739ecd2f7b6610c1e77f0aaa813b0f657043f9a
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-01-31 18:58:53 +0530

    Add error handler to blueprint.

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -3,9 +3,9 @@ from app import app, db
 
 @app.errorhandler(404)
 def not_found_error(error):
-    return render_template('404.html'), 404
+    return render_template('errors/404.html'), 404
 
 @app.errorhandler(500)
 def internal_error(error):
     db.session.rollback()
-    return render_template('500.html'), 500
+    return render_template('errors/500.html'), 500
