761095a84808c57fc8c74f65d031b74c0b82aac8
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-02-03 21:50:17 +0530

    Use new config in models.

--- app/models.py
+++ app/models.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 from time import time
+from hashlib import md5
 import jwt
-from app import app, db, login
+from flask import current_app
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
-from hashlib import md5
+from app import db, login
 
 followers = db.Table('followers',
     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
@@ -61,12 +62,12 @@ class User(UserMixin, db.Model):
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
             {'reset_password': self.id, 'exp': time() + expires_in},
-            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+            current_app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
 
     @staticmethod
     def verify_reset_password_token(token):
         try:
-            id = jwt.decode(token, app.config['SECRET_KEY'],
+            id = jwt.decode(token, current_app.config['SECRET_KEY'],
             algorithms='HS256')['reset_password']
         except:
             return
