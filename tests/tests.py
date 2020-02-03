904c16441d2c95a05622d94314d45df2fcb66fce
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-02-03 12:44:48 +0530

    Use new config in tests.

--- tests/tests.py
+++ tests/tests.py
@@ -1,16 +1,24 @@
 from datetime import datetime, timedelta
 import unittest
-from app import app, db
+from app import create_app, db
 from app.models import User, Post
+from config import Config
+
+class TestConfig(Config):
+    TESTING = True
+    SQL_DATABASE_URI = 'sqlite://'
 
 class UserModelCase(unittest.TestCase):
     def setUp(self):
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+        self.app = create_app(TestConfig)
+        self.app_context = self.app.app_context()
+        self.app_context.push()
         db.create_all()
 
     def tearDown(self):
         db.session.remove()
         db.drop_all()
+        self.app_context.pop()
 
     def test_password_hashing(self):
         u = User (username='abhishek')
