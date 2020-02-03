5194cc87aac2dea16fb140d6884f3da8a4e2e5ad
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-02-03 23:05:56 +0530

    Register blueprints.

--- app/__init__.py
+++ app/__init__.py
@@ -19,8 +19,6 @@ mail = Mail()
 bootstrap = Bootstrap()
 moment = Moment()
 
-from app.errors import bp as errors_bp
-app.register_blueprint(errors_bp)
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -32,6 +30,16 @@ def create_app(config_class=Config):
     mail.init_app(app)
     bootstrap.init_app(app)
     moment.init_app(app)
+
+
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
+
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
 
     if not app.debug and not app.testing:
 
