44973d21541368e4d6015f88608d3a896a1cb322
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-02-09 20:54:43 +0530

    Add searchable attribute to posts.

--- app/models.py
+++ app/models.py
@@ -79,6 +79,7 @@ def load_user(id):
 
 
 class Post(db.Model):
+    __searchable__ = ['body']
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
