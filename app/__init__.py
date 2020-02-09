f4f5e6a9074850c162155b6b864d50da51095499
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-02-09 19:38:32 +0530

    Update init.

--- app/__init__.py
+++ app/__init__.py
@@ -24,7 +24,7 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
 
     db.init_app(app)
-    migrate.init_app(app)
+    migrate.init_app(app, db)
     login.init_app(app)
     mail.init_app(app)
     bootstrap.init_app(app)
