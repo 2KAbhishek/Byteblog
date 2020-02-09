67e66fb94b1f0b36860ffc5956fb25c6f5e23e0a
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-02-09 23:50:43 +0530

    Move search form.

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -36,13 +36,3 @@ class ResetPasswordRequestForm(FlaskForm):
 class ResetPasswordForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
-
-class SearchForm(FlaskForm):
-    q = StringField('Search', validators=[DataRequired()])
-
-    def __init__(self, *args, **kwargs):
-        if 'formdata' not in kwargs:
-            kwargs['formdata'] = request.args
-        if 'csrf_enabled' not in kwargs:
-            kwargs['csrf_enabled'] = False
-        super(SearchForm, self)..__init__(*args, **kwargs)
--- app/main/forms.py
+++ app/main/forms.py
@@ -22,3 +22,14 @@ class EditProfileForm(FlaskForm):
 class PostForm(FlaskForm):
     post = TextAreaField('What\'s byting you?', validators=[DataRequired(), Length(min=1,max=140)])
     submit = SubmitField('Share')
+
+
+class SearchForm(FlaskForm):
+    q = StringField('Search', validators=[DataRequired()])
+
+    def __init__(self, *args, **kwargs):
+        if 'formdata' not in kwargs:
+            kwargs['formdata'] = request.args
+        if 'csrf_enabled' not in kwargs:
+            kwargs['csrf_enabled'] = False
+        super(SearchForm, self).__init__(*args, **kwargs)
