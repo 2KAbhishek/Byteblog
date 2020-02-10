b50e7abc1a9b215c2a1ae0ca0fbb42edf3481735
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-02-10 12:20:43 +0530

    Fix typos.

--- app/models.py
+++ app/models.py
@@ -87,7 +87,7 @@ class SearchableMixin(object):
         when = []
         for i in range(len(ids)):
             when.append((ids[i],[i]))
-        return cls.query.filter_by(cls.id.in_(ids)).order_by(
+        return cls.query.filter(cls.id.in_(ids)).order_by(
         db.case(when, value=cls.id)), total
 
     @classmethod
