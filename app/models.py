4dedfb2511bd137f34097bbca4990fbe38fa6b6f
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-02-14 14:31:22 +0530

    Add messages model.

--- app/models.py
+++ app/models.py
@@ -128,3 +128,13 @@ class Post(SearchableMixin, db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Message {}>'.format(self.body)
