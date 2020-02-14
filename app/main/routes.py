f0b52e55bbf1becd26598a6d0abbbc13a22592df
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-02-14 12:29:25 +0530

    Add user popups route.

--- app/main/routes.py
+++ app/main/routes.py
@@ -55,6 +55,12 @@ def user(username):
         if posts.has_prev else None
     return render_template('user.html', user=user, posts=posts.items, next_url=next_url, prev_url=prev_url)
 
+@bp.route('/user/<username>/popup')
+@login_required
+def user_popup(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user_popup.html', user=user)
+
 @bp.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
