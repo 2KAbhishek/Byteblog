dea7a741065abc170d7ae5805d2b0ab2b92a8649
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-02-14 14:59:15 +0530

    Add send message route.

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 from flask import render_template, flash, redirect, url_for, request, current_app, g
 from flask_login import current_user, login_required
-from app.main.forms import EditProfileForm, PostForm, SearchForm
-from app.models import User, Post
+from app.main.forms import EditProfileForm, PostForm, SearchForm, MessageForm
+from app.models import User, Post, Message
 from app.main import bp
 from app import db
 
@@ -119,3 +119,18 @@ def search():
     prev_url = url_for('main.search', q=g.search_form.q.data, page=page - 1) if page > 1 else None
     return render_template('search.html', title='Search', posts=posts,
                            next_url=next_url, prev_url=prev_url)
+
+@bp.route('/send_message/<recipient>', methods=['GET', 'POST'])
+@login_required
+def send_message(recipient):
+    user = User.query.filter_by(username=recipient).first_or_404()
+    form = MessageForm()
+    if form.validate_on_submit():
+        msg = Message(author=current_user, recipient=user,
+                      body=form.message.data)
+        db.session.add(msg)
+        db.session.commit()
+        flash(_('Your message has been sent.'))
+        return redirect(url_for('main.user', username=recipient))
+    return render_template('send_message.html', title=_('Send Message'),
+                           form=form, recipient=recipient)
