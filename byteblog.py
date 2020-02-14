39eb5454f8bf29a5da1eecaa9f81e7f733dfd6dc
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-02-14 16:54:53 +0530

    Add messages and notifications to shell context

--- byteblog.py
+++ byteblog.py
@@ -1,11 +1,11 @@
 from app import create_app, db
-from app.models import User, Post
+from app.models import User, Post, Notification, Message
 
 app = create_app()
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post}
+    return {'db': db, 'User': User, 'Post': Post, 'Message': Message, 'Notification': Notification}
 
 # For debugging in VS Code
 if __name__ == '__main__':
