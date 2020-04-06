a2e43f9490c274144313d07fd527a933b9d52bcd
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-04-06 18:23:49 +0530

    Add user API resource placeholders.

--- app/api/users.py
+++ app/api/users.py
@@ -1,1 +1,25 @@
 from app.api import bp
+
+@bp.route('/users/<int:id>', methods={'GET'})
+def get_user(id):
+    pass
+
+@bp.route('/users', methods=['GET'])
+def get_users():
+    pass
+
+@bp.route('/users/<int:id>/followers', methods=['GET'])
+def get_followers(id):
+    pass
+
+@bp.route('/users/<int:id>/followed', methods=['GET'])
+def get_followed(id):
+    pass
+
+@bp.route('/users', methods=['POST'])
+def create_user():
+    pass
+
+@bp.route('/users/<int:id>', methods=['PUT'])
+def update_user(id):
+    pass
