0b47a3875f8a8b6d4028588bbcc0d82012074744
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-04-07 01:53:59 +0530

    Add update_user definitions.

--- app/api/users.py
+++ app/api/users.py
@@ -52,4 +52,16 @@ def create_user():
 
 @bp.route('/users/<int:id>', methods=['PUT'])
 def update_user(id):
-    pass
+    user = User.query.get_or_404(id)
+    data = request.get_json() or {}
+    if 'username' in data and data['username'] != user.username and \
+            User.query.filter_by(username=data['username']).first():
+        return bad_request('please use a different username')
+
+    if 'email' in data and data['email'] != user.email and \
+            User.query.filter_by(email=data['email']).first():
+        return bad_request('please use a different email address')
+
+    user.from_dict(data, new_user=False)
+    db.session.commit()
+    return jsonify(user.to_dict())
