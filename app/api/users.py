8ee322b9c7f3efd3b3eddb1f013881bf8e29c544
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-04-07 00:36:24 +0530

    Add get_users definitions.

--- app/api/users.py
+++ app/api/users.py
@@ -1,5 +1,5 @@
 from app.api import bp
-from flask import jsonify
+from flask import jsonify, request
 from app.models import User
 
 @bp.route('/users/<int:id>', methods={'GET'})
@@ -8,7 +8,10 @@ def get_user(id):
 
 @bp.route('/users', methods=['GET'])
 def get_users():
-    pass
+    page = request.args.get('page', 1, type=int)
+    per_page = min(request.args.get('per_page', 10, type=int), 100)
+    data = User.to_collection_dict(User.query, page, per_page, 'api.get_users')
+    return jsonify(data)
 
 @bp.route('/users/<int:id>/followers', methods=['GET'])
 def get_followers(id):
