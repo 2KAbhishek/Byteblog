63de04c757a276dfbf040216fcd137510933c9ff
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-04-07 01:50:27 +0530

    Add create_user definitions.

--- app/api/users.py
+++ app/api/users.py
@@ -1,5 +1,7 @@
 from app.api import bp
-from flask import jsonify, request
+from app.api.errors import bad_request
+from app import db
+from flask import jsonify, request, url_for
 from app.models import User
 
 @bp.route('/users/<int:id>', methods={'GET'})
@@ -32,7 +34,21 @@ def get_followed(id):
 
 @bp.route('/users', methods=['POST'])
 def create_user():
-    pass
+    data = request.get_json() or {}
+    if 'username' not in data or 'email' not in data or 'password' not in data:
+        return bad_request('must include username, email and password fields')
+    if User.query.filter_by(username=data['username']).first():
+        return bad_request('please use a different username')
+    if User.query.filter_by(email=data['email']).first():
+        return bad_request('please use a different email address')
+    user = User()
+    user.from_dict(data, new_user=True)
+    db.session.add(user)
+    db.session.commit()
+    response = jsonify(user.to_dict())
+    response.status_code = 201
+    response.headers['Location'] = url_for('api.get_user', id=user.id)
+    return response
 
 @bp.route('/users/<int:id>', methods=['PUT'])
 def update_user(id):
