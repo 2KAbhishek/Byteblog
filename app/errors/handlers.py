4dcfa289f613a28a22f9d5924b7c1ed5cc9ab0e9
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-04-07 02:57:15 +0530

    Update error handlers.

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -7,11 +7,15 @@ def wants_json_response():
     return request.accept_mimetypes['application/json'] >= \
         request.accept_mimetypes['text/html']
 
-@bp.errorhandler(404)
+@bp.app_errorhandler(404)
 def not_found_error(error):
+    if wants_json_response():
+        return api_error_response(404)
     return render_template('errors/404.html'), 404
 
-@bp.errorhandler(500)
+@bp.app_errorhandler(500)
 def internal_error(error):
     db.session.rollback()
+    if wants_json_response():
+        return api_error_response(500)
     return render_template('errors/500.html'), 500
