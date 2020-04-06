6fb16818d9f2c42c6904e098338f9910b39f9722
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-04-06 21:51:51 +0530

    Add PaginatedAPIMixin to user model.

--- app/models.py
+++ app/models.py
@@ -14,7 +14,7 @@ followers = db.Table('followers',
     db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
 )
 
-class User(UserMixin, db.Model):
+class User(PaginatedAPIMixin, UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
