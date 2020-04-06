300ab237b32a63aceb8e075788c5a2fa96e6990e
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-04-07 02:56:21 +0530

    Add wants_json_response for content negotiation.

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -1,6 +1,11 @@
-from flask import render_template
+from flask import render_template, request
 from app import db
 from app.errors import bp
+from app.api.errors import error_response as api_error_response
+
+def wants_json_response():
+    return request.accept_mimetypes['application/json'] >= \
+        request.accept_mimetypes['text/html']
 
 @bp.errorhandler(404)
 def not_found_error(error):
