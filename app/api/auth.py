9783f235aba4a9f70d41c6ae0e43b46db6844f89
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-04-07 02:46:07 +0530

    Add verify token for token auth.

--- app/api/auth.py
+++ app/api/auth.py
@@ -1,9 +1,10 @@
 from flask import g
-from flask_httpauth import HTTPBasicAuth
+from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth
 from app.models import User
 from app.api.errors import error_response
 
 basic_auth = HTTPBasicAuth()
+token_auth = HTTPTokenAuth()
 
 @basic_auth.verify_password
 def verify_password(username, password):
@@ -16,3 +17,8 @@ def verify_password(username, password):
 @basic_auth.error_handler
 def basic_auth_error():
     return error_response(401)
+
+@token_auth.verify_token
+def verify_token(token):
+    g.current_user = User.check_token(token) if token else None
+    return g.current_user is not None
