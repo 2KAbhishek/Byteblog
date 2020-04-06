2e0ad39c57375d1776393d50002dd1fdefee5fcb
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-04-07 02:53:31 +0530

    Add revoke token.

--- app/api/tokens.py
+++ app/api/tokens.py
@@ -1,7 +1,7 @@
 from flask import jsonify, g
 from app import db
 from app.api import bp
-from app.api.auth import basic_auth
+from app.api.auth import basic_auth, token_auth
 
 @bp.route('/tokens', methods=['POST'])
 @basic_auth.login_required
@@ -10,5 +10,9 @@ def get_token():
     db.session.commit()
     return jsonify({'token': token})
 
-def remove_token():
-    pass
+@bp.route('/tokens', methods=['DELETE'])
+@token_auth.login_required
+def revoke_token():
+    g.current_user.revoke_token()
+    db.session.commit()
+    return '', 204
