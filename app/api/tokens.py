7adb13c5a6ab522971b97a13d41d4b8f0b438ab8
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-04-07 02:34:00 +0530

    Add generate token method.

--- app/api/tokens.py
+++ app/api/tokens.py
@@ -1,5 +1,14 @@
+from flask import jsonify, g
+from app import db
+from app.api import bp
+from app.api.auth import basic_auth
+
+@bp.route('/tokens', methods=['POST'])
+@basic_auth.login_required
 def get_token():
-    pass
+    token = g.current_user.get_token()
+    db.session.commit()
+    return jsonify({'token': token})
 
 def remove_token():
     pass
