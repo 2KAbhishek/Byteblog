d82be18fdf429549ba5c81988d5ab52df1cc255d
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-04-06 23:34:54 +0530

    Add get_user definitions.

--- app/api/users.py
+++ app/api/users.py
@@ -1,8 +1,10 @@
 from app.api import bp
+from flask import jsonify
+from app.models import User
 
 @bp.route('/users/<int:id>', methods={'GET'})
 def get_user(id):
-    pass
+    return jsonify(User.query.get_or_404(id).to_dict())
 
 @bp.route('/users', methods=['GET'])
 def get_users():
