27443ce8aa3903bea2c7d3350d481deec06a86f6
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-04-07 02:27:31 +0530

    Add passowrd verification to API auth.

--- app/api/auth.py
+++ app/api/auth.py
@@ -5,3 +5,10 @@ from app.api.errors import error_response
 
 basic_auth = HTTPBasicAuth()
 
+@basic_auth.verify_password
+def verify_password(username, password):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        return False
+    g.current_user = user
+    return user.check_password(password)
